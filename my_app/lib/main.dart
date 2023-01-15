4b1f736f53216b68f2b253663623d02354462610
dorothy <dorothyyuan@rocketmail.com>
2023-01-15 09:44:31 +0800

    removed unused var

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -58,8 +58,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
