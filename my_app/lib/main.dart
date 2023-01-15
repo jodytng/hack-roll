33bff07cf9406aa3f1f904fb4e4023c2cf71a11b
dorothy <dorothyyuan@rocketmail.com>
2023-01-15 09:43:25 +0800

    added landing page + telebot caskets img

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,40 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hovering/hovering.dart';
 import 'package:my_app/animated_cursor.dart';
 
-void main() => runApp(const MyApp());
+//Initialize Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) =>
+          const AnimatedCursor(child: HomeScreen(title: 'Everybody Dies')),
+    ),
+    GoRoute(
+      path: '/:userid',
+      builder: (context, state) =>
+          AnimatedCursor(child: UserScreen(userId: state.params['userid']!)),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Everybody Dies',
-        theme: ThemeData(
-          backgroundColor: const Color.fromARGB(255, 234, 224, 213),
-          primaryColor: const Color.fromARGB(255, 10, 9, 8),
-          secondaryHeaderColor: const Color.fromARGB(255, 94, 80, 63),
-          fontFamily: 'Vollkorn',
-          textTheme: const TextTheme(
-              titleLarge: TextStyle(
-                  fontSize: 32.0,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold),
-              labelMedium:
-                  TextStyle(fontSize: 16.0, fontWeight: FontWeight.w700)),
-        ),
-        debugShowCheckedModeBanner: false,
-        home: AnimatedCursor(
-            child: MyHomePage(
-          title: 'Everybody Dies',
-        )));
+    return MaterialApp.router(
+      title: 'Everybody Dies',
+      theme: ThemeData(
+        backgroundColor: const Color.fromARGB(255, 234, 224, 213),
+        primaryColor: const Color.fromARGB(255, 10, 9, 8),
+        secondaryHeaderColor: const Color.fromARGB(255, 94, 80, 63),
+        fontFamily: 'Vollkorn',
+        textTheme: const TextTheme(
+            titleLarge: TextStyle(
+                fontSize: 32.0,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+            labelMedium:
+                TextStyle(fontSize: 16.0, fontWeight: FontWeight.w700)),
+      ),
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key, required String title}) : super(key: key);
 
-  const MyHomePage({super.key, required this.title});
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+            toolbarHeight: 100,
+            backgroundColor: Theme.of(context).backgroundColor,
+            flexibleSpace: Column(children: [
+              const SizedBox(
+                height: 16,
+              ),
+              Text('everybody dies',
+                  style: Theme.of(context).textTheme.titleLarge),
+              Text('an online funeral planner',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .copyWith(color: Theme.of(context).secondaryHeaderColor)),
+            ])),
+        body: Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/backgrounds/Untitled.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(400, 300, 400, 200),
+              child: TextFormField(
+                onFieldSubmitted: (userid) {
+                  context.go('/$userid');
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 3,
+                        color: Theme.of(context).secondaryHeaderColor),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  hintText: 'Enter Telegram Username',
+                ),
+              ),
+            )));
+  }
+}
+
+class UserScreen extends StatelessWidget {
+  const UserScreen({required this.userId, Key? key}) : super(key: key);
+
+  final String userId;
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +127,7 @@ class MyHomePage extends StatelessWidget {
             ),
             Text('everybody dies',
                 style: Theme.of(context).textTheme.titleLarge),
-            Text('in loving memory of XXX',
+            Text('in loving memory of $userId',
                 style: Theme.of(context)
                     .textTheme
                     .labelMedium!
@@ -76,7 +152,8 @@ class MyHomePage extends StatelessWidget {
                     'images/placeholder.png',
                   ),
                   onHover: (event) {
-                    showDialog(context: context, builder: (_) => ShowPhoto());
+                    showDialog(
+                        context: context, builder: (_) => const ShowPhoto());
                   },
                   child: Image.asset(
                     scale: height * 0.0038,
